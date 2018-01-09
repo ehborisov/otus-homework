1a6378d79a15eb059c23d70c3702c97db54957c7
ehborisov <ehborisov@gmail.com>
2018-01-09 02:52:12 -0800

    on the review

--- hw3/scoring_server/api.py
+++ hw3/scoring_server/api.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import abc
 import hashlib
 import json
 import logging
@@ -9,8 +10,13 @@ import uuid
 from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 from datetime import datetime
 from optparse import OptionParser
+from abc import abstractproperty, abstractmethod
 
 import scoring
+
+CLIENTS_INTERESTS_METHOD = 'clients_interests'
+
+ONLINE_SCORE_METHOD = "online_score"
 
 MAX_AGE = 70
 AT_SYMBOL = '@'
@@ -44,7 +50,7 @@ GENDERS = {
 DATE_PATTERN = '%d.%m.%Y'
 
 
-class Checked(type):
+class WithCheckedFields(type):
     def __new__(mcs, clsname, bases, methods):
         required_fields = set()
         known_fields = set()
@@ -54,16 +60,33 @@ class Checked(type):
                 known_fields.add(key)
                 if getattr(value, 'is_required', True):
                     required_fields.add(key)
+                conditions = []
+                for base_class in value.__class__.__mro__:
+                    declared_conditions = getattr(base_class, 'conditions', [])
+                    if isinstance(declared_conditions, list):
+                        conditions.extend(declared_conditions)
+                setattr(value, 'conditions', conditions)
         inst = type.__new__(mcs, clsname, bases, methods)
         inst._required = required_fields
         inst._known_fields = known_fields
         return inst
 
 
+class WithAbcAndCheckedFields(abc.ABCMeta, WithCheckedFields):
+    pass
+
+
 class CheckedRequestField(object):
+    __metaclass__ = abc.ABCMeta
+
+    @abstractproperty
+    def conditions(self):
+        raise NotImplementedError(
+            'Subclasses of CheckedRequestField should declare conditions'
+            ' for value checking.')
+
     def __init__(self, **kwargs):
         self.name = None
-        self._conditions = []
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -74,112 +97,81 @@ class CheckedRequestField(object):
         is_nullable = getattr(self, 'is_nullable', False)
         if not is_nullable and value is None:
             raise ValueError("'%s' request param is not nullable!" % self.name)
-        for condition, message in self._conditions:
+        for condition, message in self.conditions:
             if not condition(value):
                 raise ValueError(message % self.name)
         instance.__dict__[self.name] = value
 
 
 class CharField(CheckedRequestField):
-    def __init__(self, **kwargs):
-        super(CharField, self).__init__(**kwargs)
-        self._conditions.extend(
-            [
-                (lambda value: isinstance(value, basestring),
-                 "'%s' request param must be of a char type"),
-            ]
-        )
+    conditions = [
+        (lambda value: isinstance(value, basestring),
+         "'%s' request param must be of a char type")
+    ]
 
 
 class ArgumentsField(CheckedRequestField):
-    def __init__(self, **kwargs):
-        super(ArgumentsField, self).__init__(**kwargs)
-        self._conditions.extend(
-            [
-                (lambda value: isinstance(value, dict),
-                 "'%s' request param must be a valid json object"),
-            ]
-        )
+    conditions = [
+        (lambda value: isinstance(value, dict),
+         "'%s' request param must be a valid json object"),
+    ]
 
 
 class EmailField(CharField):
-    def __init__(self, **kwargs):
-        super(EmailField, self).__init__(**kwargs)
-        self._conditions.extend(
-            [
-                (lambda value: AT_SYMBOL in value,
-                 "'%s' request param must be a sting containing '@' symbol."),
-            ]
-        )
+    conditions = [
+        (lambda value: AT_SYMBOL in value,
+         "'%s' request param must be a sting containing '@' symbol."),
+    ]
 
 
 class PhoneField(CheckedRequestField):
-    def __init__(self, **kwargs):
-        super(PhoneField, self).__init__(**kwargs)
-        self._conditions.extend(
-            [
-                (lambda value: isinstance(value, basestring) or
-                 isinstance(value, numbers.Number),
-                 "'%s' request field must be of a number or a char type."),
-                (lambda value: len(str(value)) == PHONE_LENGTH,
-                 "'%s' request field must contain a valid 11 digit phone."),
-                (lambda value: str(value).startswith(PHONE_PREFIX),
-                 "'%s' request field must be a phone number starting with 7"),
-            ]
-        )
+    conditions = [
+        (lambda value: isinstance(value, basestring) or
+                       isinstance(value, numbers.Number),
+         "'%s' request field must be of a number or a char type."),
+        (lambda value: len(str(value)) == PHONE_LENGTH,
+         "'%s' request field must contain a valid 11 digit phone."),
+        (lambda value: str(value).startswith(PHONE_PREFIX),
+         "'%s' request field must be a phone number starting with 7"),
+    ]
 
 
 class DateField(CheckedRequestField):
-    def __init__(self, **kwargs):
-        super(DateField, self).__init__(**kwargs)
-        self._conditions.extend(
-            [
-                (lambda v: datetime.strptime(v, DATE_PATTERN),
-                 "'%s' request field must be a date in DD.MM.YYYY format"),
-            ]
-        )
+    conditions = [
+        (lambda v: datetime.strptime(v, DATE_PATTERN),
+         "'%s' request field must be a date in DD.MM.YYYY format"),
+    ]
 
 
 class BirthDayField(DateField):
-    def __init__(self, **kwargs):
-        super(BirthDayField, self).__init__(**kwargs)
-        self._conditions.extend(
-            [
-                (lambda v: datetime.now().year -
-                 datetime.strptime(v, DATE_PATTERN).year <= MAX_AGE,
-                 "'%s' request field is invalid - too old ʕ •ᴥ•ʔ╭∩╮."),
-            ]
-        )
+    conditions = [
+        (lambda v: datetime.now().year -
+                   datetime.strptime(v, DATE_PATTERN).year <= MAX_AGE,
+         "'%s' request field is invalid - too old ʕ •ᴥ•ʔ╭∩╮."),
+    ]
 
 
 class GenderField(CheckedRequestField):
-    def __init__(self, **kwargs):
-        super(GenderField, self).__init__(**kwargs)
-        self._conditions.extend(
-            [
-                (lambda value: isinstance(value, numbers.Number),
-                 "'%s' request field must be a number."),
-                (lambda value: value in GENDERS.keys(),
-                 "'%s' request field is an unknown gender code."),
-            ]
-        )
+    conditions = [
+        (lambda value: isinstance(value, numbers.Number),
+         "'%s' request field must be a number."),
+        (lambda value: value in GENDERS.keys(),
+         "'%s' request field is an unknown gender code."),
+    ]
 
 
 class ClientIDsField(CheckedRequestField):
-    def __init__(self, **kwargs):
-        super(ClientIDsField, self).__init__(**kwargs)
-        self._conditions.extend(
-            [
-                (lambda value: isinstance(value, list),
-                 "'%s' request field must be a list."),
-                (lambda value: any(isinstance(x, numbers.Number)
-                                   for x in value),
-                 "'%s' request field must contain only numeric ids."),
-            ]
-        )
+    conditions = [
+        (lambda value: isinstance(value, list),
+         "'%s' request field must be a list."),
+        (lambda value: any(isinstance(x, numbers.Number)
+                           for x in value),
+         "'%s' request field must contain only numeric ids."),
+    ]
 
 
 class BaseRequest(object):
+    __metaclass__ = WithAbcAndCheckedFields
     _required = set()
     _known_fields = set()
 
@@ -194,15 +186,25 @@ class BaseRequest(object):
             else:
                 logging.info('Got unknown param in request: %s=%s', param, arg)
 
+    @abstractmethod
+    def handle(self, base_request, ctx, store):
+        raise NotImplementedError('Subclasses of BaseRequest should implement '
+                                  'handle method.')
+
 
 class ClientsInterestsRequest(BaseRequest):
-    __metaclass__ = Checked
     client_ids = ClientIDsField(required=True)
     date = DateField(required=False, nullable=True)
 
+    def handle(self, base_request, ctx, store):
+        ctx['nclients'] = len(self.client_ids)
+        response = {}
+        for client_id in self.client_ids:
+            response[client_id] = scoring.get_interests(store, client_id)
+        return response, OK
+
 
 class OnlineScoreRequest(BaseRequest):
-    __metaclass__ = Checked
     first_name = CharField(required=False, nullable=True)
     last_name = CharField(required=False, nullable=True)
     email = EmailField(required=False, nullable=True)
@@ -210,9 +212,19 @@ class OnlineScoreRequest(BaseRequest):
     birthday = BirthDayField(required=False, nullable=True)
     gender = GenderField(required=False, nullable=True)
 
+    def handle(self, base_request, ctx, store):
+        ctx['has'] = base_request.arguments.keys()
+        if base_request.is_admin:
+            score = 42
+        else:
+            score = scoring.get_score(store, self.phone, self.email,
+                                      self.birthday, self.gender,
+                                      self.first_name, self.last_name)
+        response = {'score': score}
+        return response, OK
+
 
 class MethodRequest(BaseRequest):
-    __metaclass__ = Checked
     account = CharField(required=False, nullable=True)
     login = CharField(required=True, nullable=True)
     token = CharField(required=True, nullable=True)
@@ -223,45 +235,37 @@ class MethodRequest(BaseRequest):
     def is_admin(self):
         return self.login == ADMIN_LOGIN
 
+    def check_auth(self):
+        if self.login == ADMIN_LOGIN:
+            digest = hashlib.sha512(datetime.now()
+                                    .strftime(
+                "%Y%m%d%H") + ADMIN_SALT).hexdigest()
+        else:
+            digest = hashlib.sha512(self.account +
+                                    self.login + SALT).hexdigest()
+        if digest == self.token:
+            return True
+        return False
 
-def check_auth(request):
-    if request.login == ADMIN_LOGIN:
-        digest = hashlib.sha512(datetime.now()
-                                .strftime("%Y%m%d%H") + ADMIN_SALT).hexdigest()
-    else:
-        digest = hashlib.sha512(request.account +
-                                request.login + SALT).hexdigest()
-    if digest == request.token:
-        return True
-    return False
-
-
-def online_score_handler(request, ctx, store):
-    s_request = OnlineScoreRequest(**request['body'].arguments)
-    ctx['has'] = request['body'].arguments.keys()
-    if request['body'].is_admin:
-        score = 42
-    else:
-        score = scoring.get_score(store, s_request.phone, s_request.email,
-                                  s_request.birthday, s_request.gender,
-                                  s_request.first_name, s_request.last_name)
-    response = {"score": score}
-    return response, OK
+    def handle(self, base_request, ctx, store):
+        if not self.check_auth():
+            return 'Invalid token', FORBIDDEN
+        if self.method == CLIENTS_INTERESTS_METHOD:
+            method_request = ClientsInterestsRequest(**self.arguments)
+        elif self.method == ONLINE_SCORE_METHOD:
+            method_request = OnlineScoreRequest(**self.arguments)
+        else:
+            return 'Unknown method', INVALID_REQUEST
+        return method_request.handle(self, ctx, store)
 
 
-def clients_interests_handler(request, ctx, store):
-    ci_request = ClientsInterestsRequest(**request['body'].arguments)
-    ctx['nclients'] = len(ci_request.client_ids)
-    response = {}
-    for client_id in ci_request.client_ids:
-        response[client_id] = scoring.get_interests(store, client_id)
-    return response, OK
+def main_method_handler(request_params, ctx, store):
+    return MethodRequest(**request_params['body']).handle(None, ctx, store)
 
 
 class MainHTTPHandler(BaseHTTPRequestHandler):
     router = {
-        "online_score": online_score_handler,
-        'clients_interests': clients_interests_handler
+        "method": main_method_handler
     }
     store = None
 
@@ -269,33 +273,26 @@ class MainHTTPHandler(BaseHTTPRequestHandler):
         return headers.get('HTTP_X_REQUEST_ID', uuid.uuid4().hex)
 
     def do_POST(self):
-
         context = {"request_id": self.get_request_id(self.headers)}
-        request = None
-        data_string = None
         response, code = {}, OK
         try:
             data_string = self.rfile.read(int(self.headers['Content-Length']))
-            request = MethodRequest(**json.loads(data_string))
-        except (ValueError, TypeError) as e:
-            code = INVALID_REQUEST
-            response = e.message
+            request_dict = json.loads(data_string)
         except Exception:
-            code = BAD_REQUEST
+            self.handle_response(BAD_REQUEST, {}, context)
+            return
         if data_string:
             logging.info("%s: %s %s" % (self.path, data_string,
                                         context["request_id"]))
-        if request:
-            if not check_auth(request):
-                self.handle_response(FORBIDDEN, response, context)
-                return
-
-            if request.method in self.router:
+        if request_dict:
+            path = self.path.strip("/")
+            if path in self.router:
                 try:
-                    response, code = self.router[request.method](
-                        {"body": request, "headers": self.headers}, context,
-                        self.store)
+                    response, code = self.router[path](
+                        {"body": request_dict, "headers": self.headers},
+                        context, self.store)
                 except (TypeError, ValueError) as e:
+                    logging.exception(e)
                     code = INVALID_REQUEST
                     response = e.message
                 except Exception as e:
