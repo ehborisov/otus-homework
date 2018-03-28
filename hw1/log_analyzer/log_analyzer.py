399bf85217e947805393599910cee973db77e988
ehborisov <ehborisov@gmail.com>
2018-03-27 22:06:55 -0700

    homework 1 final edits

--- hw1/log_analyzer/log_analyzer.py
+++ hw1/log_analyzer/log_analyzer.py
@@ -37,14 +37,6 @@ REPORT_DATE_PATTERN = '%Y.%m.%d'
 REPORT_TEMPLATE_FILE = 'report.html'
 TEMPLATE_REPLACEMENT_STRING = '$table_json'
 
-CONFIG_OPTION = '--config'
-CONFIG_LOGFILE_KEY = 'LOGFILE_PATH'
-CONFIG_TIMESTAMP_KEY = 'TIMESTAMP_PATH'
-CONFIG_LOGDIR_KEY = 'LOG_DIR'
-CONFIG_REPORT_DIR_KEY = 'REPORT_DIR'
-CONFIG_REPORT_SIZE_KEY = 'REPORT_SIZE'
-CONFIG_ERROR_THRESHOLD = 'ERROR_PERCENTAGE_THRESHOLD'
-
 REPORT_DECIMAL_FIELDS = ['count_perc', 'time_sum', 'time_perc', 'time_avg',
                          'time_max', 'time_med']
 
@@ -151,14 +143,14 @@ def build_report(config, template_path, log_entry, report_path):
                 is_zipped else open(log_entry.logfile, encoding='utf-8') as f:
             try:
                 log_lines = (line for line in f)
-                threshold = config[CONFIG_ERROR_THRESHOLD]
+                threshold = config['ERROR_PERCENTAGE_THRESHOLD']
                 request_timings = extract_data_from_log(log_lines, threshold)
             except IOError as e:
                 logging.exception('Couldn\'t read from logfile %s: %s',
                                   log_entry.logfile, e)
                 raise
             report_data = prepare_report_data(request_timings,
-                                              config[CONFIG_REPORT_SIZE_KEY])
+                                              config['REPORT_SIZE'])
             generate_report_from_template(template_path, report_path,
                                           report_data)
 
@@ -177,11 +169,11 @@ def read_config_from_file(config_path):
 
 
 def validate_configuration(config):
-    logs_dir = config[CONFIG_LOGDIR_KEY]
+    logs_dir = config['LOG_DIR']
     if not os.path.exists(logs_dir):
         raise Exception('Logs directory %s does not exist.'
                         % logs_dir)
-    report_dir = config[CONFIG_REPORT_DIR_KEY]
+    report_dir = config['REPORT_DIR']
     if not os.path.exists(report_dir):
         logging.info('Reports directory does not exist, creating a new one.')
         try:
@@ -189,16 +181,16 @@ def validate_configuration(config):
         except Exception:
             logging.error("Couldn't create reports directory.")
             raise
-    report_template = os.path.join(config[CONFIG_REPORT_DIR_KEY],
+    report_template = os.path.join(config['REPORT_DIR'],
                                    REPORT_TEMPLATE_FILE)
     if not os.path.exists(report_template):
         raise Exception('Report template file cannot be found at %s.',
                         report_template)
     try:
-        int(config[CONFIG_REPORT_SIZE_KEY])
+        int(config['REPORT_SIZE'])
     except ValueError as e:
         raise Exception('%s configuration parameter must be an'
-                        ' integer.' % CONFIG_REPORT_SIZE_KEY, e)
+                        ' integer.' % 'REPORT_SIZE', e)
 
 
 def write_timestamp_file(path):
@@ -212,10 +204,9 @@ def configure_logger(log_file_path):
     config_args = {
         'level': logging.INFO,
         'format': '[%(asctime)s] %(levelname).1s %(message)s',
-        'datefmt': '%Y.%m.%d %H:%M:%S'
+        'datefmt': '%Y.%m.%d %H:%M:%S',
+        'filename': log_file_path
     }
-    if log_file_path:
-        config_args['filename'] = log_file_path
     logging.basicConfig(**config_args)
 
 
@@ -223,7 +214,7 @@ def get_args():
     parser = argparse.ArgumentParser(
         description='Log analyzer commandline client')
     parser.add_argument('--config', nargs='?', type=str,
-                        default='/usr/local/etc/log_analyzer.conf',
+                        default='',
                         help='Path to the config file.'
                              'Default is /usr/local/etc/log_analyzer.conf')
     return parser.parse_args()
@@ -233,24 +224,26 @@ def main():
     args = get_args()
     config = DEFAULT_CONFIG.copy()
     try:
-        if args.config:
-            print('Getting config file from %s ' % args.config)
-            overrides = read_config_from_file(args.config)
-            config.update(overrides)
-        configure_logger(config[CONFIG_LOGFILE_KEY])
+        if not args.config:
+            sys.exit('Error: no configuration was passed.')
+        print('Getting config file from %s ' % args.config)
+        overrides = read_config_from_file(args.config)
+        config.update(overrides)
+        logfile_path = config.get('LOGFILE_PATH', None)
+        configure_logger(logfile_path)
         validate_configuration(config)
-        log_entry = find_latest_log_entry(config[CONFIG_LOGDIR_KEY])
+        log_entry = find_latest_log_entry(config['LOG_DIR'])
         date_str = log_entry.date.strftime(REPORT_DATE_PATTERN)
-        report_dir = config[CONFIG_REPORT_DIR_KEY]
+        report_dir = config['REPORT_DIR']
         report_path = os.path.join(report_dir, REPORT_FILE_FORMAT % date_str)
         if os.path.exists(report_path):
             logging.info('Report for the latest log entry %s already '
                          'exists.' % log_entry.logfile)
             sys.exit(0)
-        template_path = os.path.join(config[CONFIG_REPORT_DIR_KEY],
+        template_path = os.path.join(config['REPORT_DIR'],
                                      REPORT_TEMPLATE_FILE)
         build_report(config, template_path, log_entry, report_path)
-        write_timestamp_file(config[CONFIG_TIMESTAMP_KEY])
+        write_timestamp_file(config['TIMESTAMP_PATH'])
     except Exception as e:
         logging.exception(e)
         sys.exit('Log analyzer finished with error: ' + str(e))
